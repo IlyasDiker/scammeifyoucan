7eb58385898cf56cc0d54f5e7878aa7c5579813e
IlyasDiker <ilyasbenhssine@gmail.com>
2023-02-12 00:34:29 +0100

    change type to website

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@ useHead({
         {name:'robots', content:'index, nofollow'},
         {name:'description', content:'Get lost in a captivating world of con artists and learn valuable lessons on avoiding scams in the future with this must-read book.'},
         {name:'og:url', content: 'https://scammeifyoucan.vercel.app/'},
-        {name:'og:type', content: 'book'},
+        {name:'og:type', content: 'website'},
         {name:'og:title', content: '"How to Scam People" | Best seller book of the year.'},
         {name:'og:description', content: 'Get lost in a captivating world of con artists and learn valuable lessons on avoiding scams in the future with this must-read book.'},
         {name:'og:image', content: 'https://scammeifyoucan.vercel.app/large-cover.webp'},
