fe362fd479534fe0b4a383448d19b9d5434a9a81
IlyasDiker <ilyasbenhssine@gmail.com>
2023-02-12 00:31:18 +0100

    add meta and title

--- pages/index.vue
+++ pages/index.vue
@@ -6,3 +6,24 @@
     <AuthorSection/>
     <FooterSection/>
 </template>
+
+<meta property="og:url"         content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+<meta property="og:type"        content="article" />
+<meta property=" og:title" content=" When Great Minds Don't Think Alike"/>
+<meta property=" og:description" content= "How much does culture influence creative thinking?"/>
+<meta property="og:image"       content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+
+<script setup>
+useHead({
+    title: "How to Scam People | Best seller book of the year.",
+    meta:[
+        {name:'robots', content:'index, nofollow'},
+        {name:'description', content:'Get lost in a captivating world of con artists and learn valuable lessons on avoiding scams in the future with this must-read book.'},
+        {name:'og:url', content: 'https://scammeifyoucan.vercel.app/'},
+        {name:'og:type', content: 'book'},
+        {name:'og:title', content: '"How to Scam People" | Best seller book of the year.'},
+        {name:'og:description', content: 'Get lost in a captivating world of con artists and learn valuable lessons on avoiding scams in the future with this must-read book.'},
+        {name:'og:image', content: 'https://scammeifyoucan.vercel.app/large-cover.webp'},
+    ]
+})
+</script>
